9f6eb46c9341a07ae6cb60b9c2dca0891ac4a5ac
Karl Xu <karl.yogi.xu@gmail.com>
2017-09-26 19:05:47 +0800

    production js bundle file with hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
 var path = require('path');
 var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        './src/app.js',
-    ],
-    devtool: 'source-map',
-    output: {
-        path: __dirname+'/dist',
-        filename: 'bundle.js',
-        publicPath: '/dist/',
-    },
-    module: {
-        loaders: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                loader: [
-                    'babel-loader?'+JSON.stringify({
-                        presets: ['es2015', 'react', 'stage-1', 'stage-2']
-                    })
-                ],
-            },
-            { test: /\.css$/, loader: 'style-loader!css-loader?modules' },
+  entry: [
+    'babel-polyfill',
+    './src/app.js',
+  ],
+  devtool: 'source-map',
+  output: {
+    path: __dirname+'/dist',
+    filename: 'bundle-[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.template.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: [
+          'babel-loader?'+JSON.stringify({
+            presets: ['es2015', 'react', 'stage-1', 'stage-2']
+          })
         ],
-    }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader?modules' },
+    ],
+  }
 };
